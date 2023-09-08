f3f4b0e4695653f7b2737d8d938de8255d376a9a
Jaybee-rb25 <jaybee1044@gmail.com>
2023-09-07 18:03:02 -0700

    3-name

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+
+/**
+ * main - this  the main function
+ *
+ * return: alsways 0
+ */
+
+
+int main(void)
+
+
+{ puts("\"Programming is like building a multilingual puzzle"):
+	return (0)
+}
