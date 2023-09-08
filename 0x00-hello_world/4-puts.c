dfe3abaaf8dfbb87c06b2494f4541ecdfb1ff5e2
Jaybee-rb25 <jaybee1044@gmail.com>
2023-09-07 18:42:21 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <studio.h>
 
 /**
- * main - this  the main function
+ * main - this is the main function
  *
- * return: alsways 0
+ * Return: always 0
  */
 
-
 int main(void)
-
-
-{ puts("\"Programming is like building a multilingual puzzle"):
-	return (0)
+{
+	puts("\"Programming is like building a multilingual puzzle);
+	return (0);
 }
